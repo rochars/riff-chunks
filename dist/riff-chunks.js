3e616d7bdcc6f8667647bbb122177b3777bcbd71
rochars <rocha.rafaelsilva@gmail.com>
2017-11-29 02:03:36 -0200

    getChunkId() and getChunkSize()

--- dist/riff-chunks.js
+++ dist/riff-chunks.js
@@ -440,12 +440,8 @@ const byteData = __webpack_require__(6);
  */
 function getChunks(buffer, bigEndian=false) {
     return {
-        "chunkId": byteData.fromBytes(
-                buffer.slice(0, 4), 8, {"char": true}
-            ),
-        "chunkSize": byteData.fromBytes(
-                buffer.slice(4, 8), 32, {'be': bigEndian}
-            )[0],
+        "chunkId": getChunkId(buffer, 0),
+        "chunkSize": getChunkSize(buffer, 0, bigEndian),
         "format": byteData.fromBytes(
                 buffer.slice(8, 12), 8, {"char": true}
             ),
@@ -463,10 +459,8 @@ function getSubChunks(buffer, bigEndian) {
     let chunks = [];
     let i = 12;
     while(i < buffer.length) {
-        let subChunkId = byteData.fromBytes(
-            buffer.slice(i, i + 4), 8, {"char": true});
-        let subChunkSize = byteData.fromBytes(
-            buffer.slice(i + 4, i + 8), 32, {'be': bigEndian})[0];
+        let subChunkId = getChunkId(buffer, i);
+        let subChunkSize = getChunkSize(buffer, i, bigEndian);
         if (subChunkId == "LIST") {
             chunks.push({
                     "subChunkId": subChunkId,
@@ -484,6 +478,30 @@ function getSubChunks(buffer, bigEndian) {
         i = i + 8 + subChunkSize;
     }
     return chunks;
+}
+
+/**
+ * Return the FourCC of a chunk.
+ * @param {Uint8Array|!Array<number>} buffer the RIFF file bytes.
+ * @param {number} index The start index of the chunk.
+ * @return {string}
+ */
+function getChunkId(buffer, index) {
+    return byteData.fromBytes(
+            buffer.slice(index, index + 4), 8, {"char": true});
+}
+
+/**
+ * Return the size of a chunk.
+ * @param {Uint8Array|!Array<number>} buffer the RIFF file bytes.
+ * @param {number} index The start index of the chunk.
+ * @return {number}
+ */
+function getChunkSize(buffer, index, bigEndian) {
+    return byteData.fromBytes(
+            buffer.slice(index + 4, index + 8),
+            32,
+            {'be': bigEndian, "single": true});
 }
 
 window['getChunks'] = getChunks;
--- index.js
+++ index.js
@@ -16,12 +16,8 @@ const byteData = require("byte-data");
  */
 function getChunks(buffer, bigEndian=false) {
     return {
-        "chunkId": byteData.fromBytes(
-                buffer.slice(0, 4), 8, {"char": true}
-            ),
-        "chunkSize": byteData.fromBytes(
-                buffer.slice(4, 8), 32, {'be': bigEndian}
-            )[0],
+        "chunkId": getChunkId(buffer, 0),
+        "chunkSize": getChunkSize(buffer, 0, bigEndian),
         "format": byteData.fromBytes(
                 buffer.slice(8, 12), 8, {"char": true}
             ),
@@ -39,10 +35,8 @@ function getSubChunks(buffer, bigEndian) {
     let chunks = [];
     let i = 12;
     while(i < buffer.length) {
-        let subChunkId = byteData.fromBytes(
-            buffer.slice(i, i + 4), 8, {"char": true});
-        let subChunkSize = byteData.fromBytes(
-            buffer.slice(i + 4, i + 8), 32, {'be': bigEndian})[0];
+        let subChunkId = getChunkId(buffer, i);
+        let subChunkSize = getChunkSize(buffer, i, bigEndian);
         if (subChunkId == "LIST") {
             chunks.push({
                     "subChunkId": subChunkId,
@@ -60,6 +54,30 @@ function getSubChunks(buffer, bigEndian) {
         i = i + 8 + subChunkSize;
     }
     return chunks;
+}
+
+/**
+ * Return the FourCC of a chunk.
+ * @param {Uint8Array|!Array<number>} buffer the RIFF file bytes.
+ * @param {number} index The start index of the chunk.
+ * @return {string}
+ */
+function getChunkId(buffer, index) {
+    return byteData.fromBytes(
+            buffer.slice(index, index + 4), 8, {"char": true});
+}
+
+/**
+ * Return the size of a chunk.
+ * @param {Uint8Array|!Array<number>} buffer the RIFF file bytes.
+ * @param {number} index The start index of the chunk.
+ * @return {number}
+ */
+function getChunkSize(buffer, index, bigEndian) {
+    return byteData.fromBytes(
+            buffer.slice(index + 4, index + 8),
+            32,
+            {'be': bigEndian, "single": true});
 }
 
 module.exports.getChunks = getChunks;
